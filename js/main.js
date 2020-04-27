1a22afa7124296c9eca810c48067fa7e4b79e51c
Codeshots <codeshots@gmx.de>
2020-04-27 15:53:22 +0200

    Added anime.js animations.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+	anime({
+		targets: '.content-wrapper h1',
+		translateX: [-400, 0],
+		easing: 'easeOutExpo',
+		duration: 1500,
+		delay: 1000,
+		opacity: [0, 1],
+	})
+
+	anime({
+		targets: 'p',
+		translateY: [50, 0],
+		easing: 'easeOutExpo',
+		duration: 1500,
+		delay: (el, i) => {
+			return 1000 + 100 * i;
+		},
+		opacity: [0, 1],
+	})
+
+	anime({
+		targets: 'nav a',
+		translateY: [-50, 0],
+		easing: 'easeOutExpo',
+		duration: 1500,
+		delay: (el, i) => {
+			return 1500 + 100 * i;
+		},
+		opacity: [0, 1],
+	})
+
+	anime({
+		targets: '.logo',
+		translateX: [-200, 0],
+		easing: 'easeOutExpo',
+		duration: 1500,
+		delay: (el, i) => {
+			return 1000 + 100 * i;
+		},
+		opacity: [0, 1],
+	})
+
+
+	anime({
+		targets: '.left',
+		easing: 'easeOutExpo',
+		duration: 4000,
+		opacity: [0, 1],
+	})
+})
